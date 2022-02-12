f46aa00510493dfa011598903373945fc4034d6e
Taucari <89819413+Taucari@users.noreply.github.com>
2022-02-12 20:06:46 +0000

    Prelim player_list

--- config.py
+++ config.py
@@ -17,12 +17,18 @@ The only restriction is that every player name
 has to be unique and the number of players has
 to be a multiple of TEAM_SIZE
 """
-PLAYER_LIST = {'Player1': [2069, 2067],
-               'Player2': [1409, 1407],
-               'Player3': [1873, 1871],
-               'Player4': [918],
-               'Player5': [1363],
-               'Player6': [2021],
+PLAYER_LIST = {'Moira#4231': [2919, 2700, 2606, 2366, 1667],
+               'Trion#5840': [1624, 1454, 1297, 1287, 1192],
+               'Fighterman B.P.S Capt#9988 ': [2343, 1840, 1637, 1621, 1562],
+               'Kanzou#9844': [1369, 1221, 1205, 1195, 1174],
+               'graxor#9292': [1297, 1162, 1143, 978, 826],
+               'Nero321#6960': [1559, 1365, 1274, 1271, 1176],
+               'kuroneko#1765': [1448, 1409, 1070, 923],
+               'SinisteRock#8632': [1878, 1840, 1821, 1479, 1431],
+               'Crock0Dial#1100': [1489, 1416, 1314, 1241, 1207],
+               'Deadskwerl#3556': [1419, 1373, 1324, 1320, 1287],
+               'ZoulCash#9702': [1593, 1429, 1321, 1234, 1209],
+               'Stika#9712': [1979, 1958, 1696, 1485, 1381],
                }
 TEAM_SIZE = 3
 DECIMAL_PLACES = 2
