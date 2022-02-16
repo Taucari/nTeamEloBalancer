3cc83bf4bc62c1b32b6767be6eb9937e3f1b5e2f
Taucari <89819413+Taucari@users.noreply.github.com>
2022-02-16 20:56:55 +0000

    multiprocessing changes

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import itertools as it
 import pprint
 import concurrent.futures
 
+import ppprint
+
 
 def new_determine_team_combos(player_list, team_size):
     number_of_teams = int(len(player_list) / team_size)
@@ -16,10 +18,6 @@ def new_determine_team_combos(player_list, team_size):
         del new_team_combos[team_combo_no]
         tail = [actual_combos for actual_combos in new_team_combos if set(head).isdisjoint(set(actual_combos))]
         output[tuple(head)] = tail
-        # multiprocess this part
-        # output[tuple(head)] = [i for i in it.combinations(tail, number_of_teams-1) if
-        #                        len(set(it.chain(*i))) == len(list(it.chain(*i)))]
-
     return output
 
 
@@ -41,30 +39,35 @@ def multifunction(input_dict):
     output = {}
     done = 0
     for k, v in input_dict.items():
-        output[k] = [i for i in it.combinations(v, 3) if
+        output[k] = [[*i] for i in it.combinations(v, 4) if
                      len(set(it.chain(*i))) == len(list(it.chain(*i)))]
         done += 1
         print('Done ' + str(done))
-    return output
+    output_list = []
+    for k, v in output.items():
+        for micro_combo in v:
+            head = [[*k]]
+            head.extend(micro_combo)
+            output_list.append(head)
+    return output_list
 
 
 def handle_multi():
-    players = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+    # players = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
     size = 3
-    # players = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']
+    players = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']
 
     new_result = new_determine_team_combos(players, size)
     sectioned_result = split_dict_equally(new_result, chunks=20)
-
+    final_result = []
     with concurrent.futures.ProcessPoolExecutor() as executor:
         results = executor.map(multifunction, sectioned_result)
 
         for result in results:
-            pprint.pprint(result)
+            final_result.extend(result)
 
-    # pprint.pprint(sectioned_result)
-    # print(len(sectioned_result))
-    print('Number of possible combinations of teams: ' + str(len(new_result)))
+    print('Number of possible combinations of teams: ' + str(len(final_result)))
+    pprint.pprint(final_result[0:5])
 
 
 if __name__ == '__main__':
